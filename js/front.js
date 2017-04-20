bd3f6b42eda9e3878fc14c8d7e3e52786004a665
Valeria Rozenbaum <valeria.rozenbaum@gmail.com>
2017-04-20 12:03:49 -0400

    Changing Bootstrap

--- js/front.js
+++ js/front.js
@@ -1,11 +1,17 @@
-masonry();
+if ($.cookie("theme_csspath")) {
+    $('link#theme-stylesheet').attr("href", $.cookie("theme_csspath"));
+}
+if ($.cookie("theme_layout")) {
+    $('body').addClass($.cookie("theme_layout"));
+}
 
 $(function () {
 
-    offCanvas();
     lightbox();
-    carousels();
+    sliders();
+    menuSliding();
     utils();
+    map();
     demo();
 
 });
@@ -51,54 +57,6 @@ function demo() {
 }
 
 /* =========================================
- *  carousels
- *  =======================================*/
-
-function carousels() {
-
-    $('#main-slider').owlCarousel({
-	navigation: true, // Show next and prev buttons
-	slideSpeed: 300,
-	paginationSpeed: 400,
-	autoPlay: true,
-	stopOnHover: true,
-	singleItem: true,
-	afterInit: ''
-    });
-   
-}
-
-/* =========================================
- *  masonry
- *  =======================================*/
-
-function masonry() {
-
-    var $grid = $('.grid').masonry({
-        itemSelector: ".masonry-item"
-    });
-
-    $grid.imagesLoaded().progress(function () {
-        $grid.masonry('layout');
-    });
-}
-
-/* =========================================
- *  Off-canvas menu
- *  =======================================*/
-
-function offCanvas() {
-
-    $(document).ready(function () {
-        $('[data-toggle="offcanvas"]').click(function () {
-            $('.row-offcanvas').toggleClass('active')
-        });
-    });
-
-}
-
-
-/* =========================================
  *  lightbox
  *  =======================================*/
 
@@ -110,9 +68,179 @@ function lightbox() {
     });
 }
 
-/* =========================================
- *  utils
- *  =======================================*/
+/* sliders */
+
+function sliders() {
+    if ($('.owl-carousel').length) {
+
+
+        $(".testimonials").owlCarousel({
+            items: 4,
+            itemsDesktopSmall: [1100, 3],
+            itemsTablet: [768, 2],
+            itemsMobile: [480, 1]
+        });
+    }
+
+}
+
+function map() {
+
+    if ($('#map').length > 0) {
+
+
+        function initMap() {
+
+            var location = new google.maps.LatLng(50.0875726, 14.4189987);
+
+            var mapCanvas = document.getElementById('map');
+            var mapOptions = {
+                center: location,
+                zoom: 16,
+                panControl: false,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            }
+            var map = new google.maps.Map(mapCanvas, mapOptions);
+
+            var markerImage = 'img/marker.png';
+
+            var marker = new google.maps.Marker({
+                position: location,
+                map: map,
+                icon: markerImage
+            });
+
+            var contentString = '<div class="info-window">' +
+                    '<h3>Info Window Content</h3>' +
+                    '<div class="info-content">' +
+                    '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>' +
+                    '</div>' +
+                    '</div>';
+
+            var infowindow = new google.maps.InfoWindow({
+                content: contentString,
+                maxWidth: 400
+            });
+
+            marker.addListener('click', function () {
+                infowindow.open(map, marker);
+            });
+
+            var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
+
+            map.set('styles', styles);
+
+
+        }
+
+        google.maps.event.addDomListener(window, 'load', initMap);
+
+
+    }
+
+}
+
+
+
+/* menu sliding */
+
+function menuSliding() {
+
+
+    $('.dropdown').on('show.bs.dropdown', function (e) {
+
+        if ($(window).width() > 750) {
+            $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+
+        }
+        else {
+            $(this).find('.dropdown-menu').first().stop(true, true).show();
+        }
+    }
+
+    );
+    $('.dropdown').on('hide.bs.dropdown', function (e) {
+        if ($(window).width() > 750) {
+            $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+        }
+        else {
+            $(this).find('.dropdown-menu').first().stop(true, true).hide();
+        }
+    });
+
+}
+
+/* animations */
+
+function animations() {
+    delayTime = 0;
+    $('[data-animate]').css({opacity: '0'});
+    $('[data-animate]').waypoint(function (direction) {
+        delayTime += 150;
+        $(this).delay(delayTime).queue(function (next) {
+            $(this).toggleClass('animated');
+            $(this).toggleClass($(this).data('animate'));
+            delayTime = 0;
+            next();
+            //$(this).removeClass('animated');
+            //$(this).toggleClass($(this).data('animate'));
+        });
+    },
+            {
+                offset: '90%',
+                triggerOnce: true
+            });
+
+    $('[data-animate-hover]').hover(function () {
+        $(this).css({opacity: 1});
+        $(this).addClass('animated');
+        $(this).removeClass($(this).data('animate'));
+        $(this).addClass($(this).data('animate-hover'));
+    }, function () {
+        $(this).removeClass('animated');
+        $(this).removeClass($(this).data('animate-hover'));
+    });
+
+}
+
+function animationsSlider() {
+
+    var delayTimeSlider = 400;
+
+    $('.owl-item:not(.active) [data-animate-always]').each(function () {
+
+        $(this).removeClass('animated');
+        $(this).removeClass($(this).data('animate-always'));
+        $(this).stop(true, true, true).css({opacity: 0});
+
+    });
+
+    $('.owl-item.active [data-animate-always]').each(function () {
+        delayTimeSlider += 500;
+
+        $(this).delay(delayTimeSlider).queue(function (next) {
+            $(this).addClass('animated');
+            $(this).addClass($(this).data('animate-always'));
+
+            console.log($(this).data('animate-always'));
+
+        });
+    });
+
+
+
+}
+
+/* counters */
+
+function counters() {
+
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+}
 
 function utils() {
 
@@ -141,15 +269,29 @@ function utils() {
     });
     /* animated scrolling */
 
-    $('.scroll-to').click(function (event) {
-        event.preventDefault();
+    $('.scroll-to, .scroll-to-top').click(function (event) {
+
         var full_url = this.href;
         var parts = full_url.split("#");
-        var trgt = parts[1];
+        if (parts.length > 1) {
 
-        $('body').scrollTo($('#' + trgt), 800, {offset: -80});
-
+            scrollTo(full_url);
+            event.preventDefault();
+        }
     });
+    function scrollTo(full_url) {
+        var parts = full_url.split("#");
+        var trgt = parts[1];
+        var target_offset = $("#" + trgt).offset();
+        var target_top = target_offset.top - 100;
+        if (target_top < 0) {
+            target_top = 0;
+        }
+
+        $('html, body').animate({
+            scrollTop: target_top
+        }, 1000);
+    }
 }
 
 /* product detail gallery */
